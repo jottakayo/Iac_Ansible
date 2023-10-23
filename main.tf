a9d3066b69a28958d47553f8c93f45b6cb1ad98d
Jotta <jottakayoml@gmail.com>
2023-10-23 15:39:53 -0300

    Novo modelo de Projeto com ansible

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+# Declara a versão mínima do provedor AWS necessária para este código Terraform
 terraform {
   required_providers {
     aws = {
@@ -7,13 +8,22 @@ terraform {
   }
   
 }
+# Define o provedor AWS e especifica a região onde as operações serão realizadas
 provider "aws" {
   region  = "us-east-1"
 }
+# Resource para criar uma instância AWS usando uma AMI específica
 resource "aws_instance" "code_iac" {
+  # ID da AMI que será usada para a instância
   ami           = "ami-0fc5d935ebf8bc3bc"
+
+  # Tipo de instância
   instance_type = "t2.micro"
-    key_name = "iac_code_key"
+
+  # Nome da chave SSH para acessar a instância
+  key_name      = "iac_code_key"
+
+  # Tags para identificar e organizar a instância
   tags = {
     Name = "code_iac"
   }
